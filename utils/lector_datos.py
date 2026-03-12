a9ca8f5cfaf70ccdd1e22a1cf5f4d44969b88f97
Gabinarvaezp <gabrielanarvaezpardo@gmail.com>
2026-03-12 13:11:33 -0500

    Refactor data loading functions in lector_datos.py
    Removed cargar_todos_los_grupos function and added leer_tabla_pegada function.

--- utils/lector_datos.py
+++ utils/lector_datos.py
@@ -1,14 +1,4 @@
 import pandas as pd
-import os
 
-def cargar_todos_los_grupos():
-    data_path = "data"
-    archivos = [f for f in os.listdir(data_path) if f.endswith(".csv") and f.startswith("grupo")]
-    df_list = []
-
-    for archivo in archivos:
-        df = pd.read_csv(os.path.join(data_path, archivo))
-        df["Grupo"] = archivo.replace(".csv", "").replace("grupo", "")
-        df_list.append(df)
-
-    return pd.concat(df_list, ignore_index=True)
+def leer_tabla_pegada(texto):
+    return pd.read_csv(pd.io.common.StringIO(texto), sep="\t")
