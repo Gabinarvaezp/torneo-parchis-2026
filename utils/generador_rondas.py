357aa8cf417b191772e5ca7e283c48bfbeb4a990
Gabinarvaezp <gabrielanarvaezpardo@gmail.com>
2026-03-12 13:11:57 -0500

    Refactor round generation logic in generador_rondas.py
    Refactor function to generate pairs for the next round.

--- utils/generador_rondas.py
+++ utils/generador_rondas.py
@@ -1,20 +1,9 @@
+
 import pandas as pd
-import random
 
-def generar_siguiente_ronda():
-    resultados_path = "data/resultados.csv"
-    df = pd.read_csv(resultados_path, header=None, names=["Ganador"])
-
-    ganadores = df["Ganador"].tolist()
-    random.shuffle(ganadores)
-
-    nuevas_parejas = []
-
+def generar_ronda(ganadores):
+    parejas = []
     for i in range(0, len(ganadores), 2):
-        if i+1 < len(ganadores):
-            nuevas_parejas.append({
-                "Equipo A": ganadores[i],
-                "Equipo B": ganadores[i+1]
-            })
-
-    pd.DataFrame(nuevas_parejas).to_csv("data/ronda_siguiente.csv", index=False)
+        if i + 1 < len(ganadores):
+            parejas.append([ganadores[i], ganadores[i + 1]])
+    return pd.DataFrame(parejas, columns=["Equipo A", "Equipo B"])
